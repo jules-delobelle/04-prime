d92bf43b0f26a26fbb1719d239368daae6ee2e7c
Jules <61208950+jules-delobelle@users.noreply.github.com>
2024-10-24 08:49:33 +0000

    first commit

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
+"""A program for prime verifications"""
+
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    This function tell if a number is prime or not
 
-    # votre code ici
+    Args:
+        p (int): the number to be verified
 
-    pass
+    Returns:
+        premier (bool): true if p is prime, false otherwise
+
+    """
+    premier = True
+    if p == 1:
+        premier = False
+    else:
+        for k in range(2, int(sqrt(p)+1)):
+            if p % k == 0:
+                premier = False
+                break
+
+    return premier
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    Main function, execute different tests
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
